8e84811450f0cdf1394aaa2fab18bf5ab48e3608
Jordy Coder <j.hamwijk34@gmail.com>
2023-10-07 13:19:34 +0200

    Init commit

---
+++ src/main/java/nl/jdacoder/springbootdemo/MicroserviceWithSpringApplication.java
@@ -0,0 +1,13 @@
+package nl.jdacoder.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/jdacoder/springbootdemo/MicroserviceWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jdacoder.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
